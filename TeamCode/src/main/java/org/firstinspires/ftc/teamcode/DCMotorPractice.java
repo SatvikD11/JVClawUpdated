5f11d5b1dee2b2045ae6548646beaead08ea05a6
sdas026 <Santosh.das@ascension.org>
2025-10-10 10:02:51 -0500

    created basic test for dc motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DCMotorPractice.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+@TeleOp(name="Robot: Siddy-Motor", group="Robot")
+public class DCMotorPractice extends OpMode {
+
+    TestBench bench = new TestBench();
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        bench.setMotorSpeed(0.5);
+    }
+}
